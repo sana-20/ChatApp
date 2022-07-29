fb270418934f07134fd93aa848791880494b1eef
sana-20 <sana20.dev@gmail.com>
2022-07-29 10:25:14 +0900

    친구목록 usecase 리팩토링

--- app/src/main/java/com/example/chatapp/domain/GetFriendsUseCase.kt
+++ app/src/main/java/com/example/chatapp/domain/GetFriendsUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.chatapp.domain
 
-import com.example.chatapp.common.map
+import com.example.chatapp.common.zip
 import com.example.chatapp.data.FriendRepository
 import com.example.chatapp.data.remote.BaseResult
 import com.example.chatapp.domain.mapper.FriendMapper
@@ -14,11 +14,23 @@ class GetFriendsUseCase @Inject constructor(
     private val friendRepository: FriendRepository,
     private val friendMapper: FriendMapper
 ) {
-    suspend fun invoke(type: FriendType): Flow<BaseResult<List<Friend>>> {
-        return friendRepository.getFriends(type.toString()).map {
-            it.map { list ->
-                friendMapper.map(list, type)
+    suspend fun invoke(): Flow<Triple<BaseResult<List<Friend>>, BaseResult<List<Friend>>, BaseResult<List<Friend>>>> {
+        val friendsFlow = friendRepository.getFriends(FriendType.FRIENDS.toString())
+            .map {
+                friendMapper.map(it, FriendType.FRIENDS)
             }
+
+        val favoriteFlow = friendRepository.getFriends(FriendType.FAVORITE.toString())
+            .map {
+                friendMapper.map(it, FriendType.FAVORITE)
+            }
+        val recommendFlow = friendRepository.getFriends(FriendType.RECOMMEND.toString())
+            .map {
+                friendMapper.map(it, FriendType.RECOMMEND)
+            }
+
+        return zip(friendsFlow, favoriteFlow, recommendFlow) { a, b, c ->
+            Triple(a, b, c)
         }
     }
 }
--- app/src/main/java/com/example/chatapp/domain/mapper/FriendMapper.kt
+++ app/src/main/java/com/example/chatapp/domain/mapper/FriendMapper.kt
@@ -1,12 +1,20 @@
 package com.example.chatapp.domain.mapper
 
+import com.example.chatapp.common.map
+import com.example.chatapp.data.remote.BaseResult
 import com.example.chatapp.data.remote.FriendModel
 import com.example.chatapp.ui.friend.FriendType
 import com.example.chatapp.ui.friend.model.Friend
 import javax.inject.Inject
 
 class FriendMapper @Inject constructor() {
-    fun map(from: List<FriendModel>, type: FriendType): List<Friend> {
+    fun map(from: BaseResult<List<FriendModel>>, type: FriendType): BaseResult<List<Friend>> {
+        return from.map { list ->
+            map(list, type)
+        }
+    }
+
+    private fun map(from: List<FriendModel>, type: FriendType): List<Friend> {
         val list = mutableListOf<Friend>()
         val profileList = from.map {
             Friend.Profile(it.name, it.profile)
