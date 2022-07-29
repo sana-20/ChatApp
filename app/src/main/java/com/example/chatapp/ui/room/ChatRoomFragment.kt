256e915b5c5c3a4230acc79b8eefc26f3919ddd4
sana-20 <sana20.dev@gmail.com>
2022-07-29 11:35:15 +0900

    뷰가 화면에 표시될 때만 flow 데이터 수집하도록 변경

--- app/src/main/java/com/example/chatapp/ui/room/ChatRoomFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/room/ChatRoomFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatapp.databinding.FragmentChatRoomBinding
@@ -48,11 +50,13 @@ class ChatRoomFragment : Fragment(), ChatRoomViewHolder.Event {
 
     private fun observeState() {
         lifecycleScope.launch {
-            chatRoomViewModel.uiState.collect {
-                when (it) {
-                    UiState.Error -> {}
-                    UiState.Loading -> {}
-                    is UiState.Success -> adapter.submitList(it.data)
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                chatRoomViewModel.uiState.collect {
+                    when (it) {
+                        UiState.Error -> {}
+                        UiState.Loading -> {}
+                        is UiState.Success -> adapter.submitList(it.data)
+                    }
                 }
             }
         }
